e810508658a53ae5b334ba4cc23e3b2fbc6bbdec
madhusudan kumar <madhusudanshambhu@gmail.com>
2026-04-19 09:21:00 +0530

    feat: initial project setup

---
+++ main.py
@@ -0,0 +1,30 @@
+import logging
+import os
+import uuid
+import datetime
+from fastapi import FastAPI
+import inngest
+from inngest.experimental import ai
+from inngest.experimental.mocked import trigger
+import inngest.fast_api
+from dotenv import load_dotenv
+
+load_dotenv()
+
+inngest_client = inngest.Inngest(
+    app_id="chat_pdf",
+    logger=logging.getLogger("uvicorn"),
+    is_production=False,
+    serializer=inngest.PydanticSerializer()
+)
+
+@inngest_client.create_function(
+    fn_id="RAG: Ingest PDF",
+    trigger=inngest.TriggerEvent(event="rag/ingest_pdf")
+)
+async def rag_ingest_pdf(ctx: inngest.Context):
+    return {"hello": "world"}
+
+app = FastAPI()
+
+inngest.fast_api.serve(app, inngest_client, [rag_ingest_pdf])
